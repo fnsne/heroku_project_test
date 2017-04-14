81b6834a093d07440b8a4890c25d610161a945fa
fnsne <watashiwafnsne@gmail.com>
2017-04-14 17:07:46 +0800

    add cool ascii faces in \/cool

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+//below new added
+var cool = require('cool-ascii-faces');
+//up new added
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,7 +14,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+
 });
+
+//below new added
+app.get('/cool', function (request, response) {
+  response.send(cool());
+});
+//up new added
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
